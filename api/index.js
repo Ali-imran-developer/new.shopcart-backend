c65eb0e600249a54b4da8832e72f1b3a80837743
Ali-imran-developer <ai8440018@gmail.com>
2025-06-18 17:43:29 +0500

    update code

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const axios = require("axios");
 const serverless = require("serverless-http");
 require("dotenv").config();
 
