bd7ca33aa7a654a9d06f1e2b4b2747e93ec45b4a
Ali-imran-developer <ai8440018@gmail.com>
2025-06-29 03:10:04 +0500

    remove the comment line

--- api/stripe/index.js
+++ api/stripe/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { paymentIntent, webhookCreate } = require("../controllers/stripewebhook");
 const router = express.Router();
-// const protect = require("../controllers/protect");
 
 router.post("/create-payment-intent", paymentIntent);
 router.post("/webhook", express.raw({ type: "application/json" }), webhookCreate);
