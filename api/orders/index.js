b848d6c0b6827f0ee9ee1a4f9be61a426cad33f7
Ali-imran-developer <ai8440018@gmail.com>
2025-06-30 19:31:38 +0500

    update code

--- api/orders/index.js
+++ api/orders/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../controllers/protect");
-const rateLimiter  = require("../controllers/ratelimiter");
+// const rateLimiter  = require("../controllers/ratelimiter");
 const { createOrder, getAllOrder, updateOrder, deleteOrder, updateStatus, bookingOrder, getBookingOrder, getDashboardStats } = require("../controllers/order");
 
 router.post("/create", protect, createOrder);
-router.get("/get", rateLimiter(2, 1), protect, getAllOrder);
+router.get("/get", protect, getAllOrder);
 router.get("/dashboard-stats", protect, getDashboardStats);
 router.put("/update/:id", updateOrder);
 router.delete("/delete/:id", deleteOrder);
